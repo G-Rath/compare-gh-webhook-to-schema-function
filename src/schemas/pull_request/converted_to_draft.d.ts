b729a1cdbefcdbb328ad067b8f095c191f9d48ef
wolfy1339 <webmaster@wolfy1339.com>
2021-01-06 10:09:02 -0500

    Fix schema title for `pull_request.converted_to_draft`

--- src/schemas/pull_request/converted_to_draft.d.ts
+++ src/schemas/pull_request/converted_to_draft.d.ts
@@ -6,7 +6,7 @@ import { Installation, Organization, Repository, User } from '../common';
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export interface PullRequestReadyForReviewEvent {
+export interface PullRequestConvertedToDraftEvent {
   action: 'converted_to_draft';
   number: number;
   pull_request: {
